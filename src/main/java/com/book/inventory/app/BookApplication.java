379fdc591061831e0b0200d2508b1a49048bf437
Prajwal-banakar <prajwal.banakara@gmail.com>
2025-12-21 22:52:59 +0530

    docker image

--- src/main/java/com/book/inventory/app/BookApplication.java
+++ src/main/java/com/book/inventory/app/BookApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
-        System.out.println(" Application started fine in 'http://localhost:8080/' ");
+        System.out.println("Application started fine in 'http://localhost:8080/'");
 	}
-
 }
